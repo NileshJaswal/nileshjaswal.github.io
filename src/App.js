22889d4944d1a6a81e91151b10eacf522c0596de
Nilesh Jaswal <arcanenilesh@gmail.com>
2024-07-21 23:49:22 +0530

    Initialize portfolio mark2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './img/Logo.svg'
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <header>
+      <div><img src={logo} alt="Logo" /></div>
+    </header>
+   </>
+  );
+}
+
+export default App;
